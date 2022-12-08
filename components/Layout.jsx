64011e9576f736def29ed9f07ce7a10d9ed50038
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-08 15:57:31 +0000

    create layout, footer and header style

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Head from "next/head"
+import React from "react"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
 
-const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
+const Layout = ({children}) => {
+    return (
+        <div className="layout">
+            <Head>
+                <title>My Store</title>
+            </Head>
+            <header>
+                <Navbar />
+            </header>
+            <main className="main-container">{children}</main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    )
 }
 
 export default Layout
--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ const Home = ({products,bannerData}) => {
         </div>
 
         <div className='products-container'>
-          {products?.map(product=>product.name)}
+          {products?.map(product=><Product key={product._id} product={product}/>)}
         </div>
 
-        <Footer/>
+        <FooterBanner footerBanner={bannerData&&bannerData[0]}/>
     </>
   )
 }
